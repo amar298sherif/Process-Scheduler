2a55ccc28a8b544f65a69de19b5832edd967ec60
amar298sherif <117904287+amar298sherif@users.noreply.github.com>
2024-01-25 21:11:08 +0200

    new

--- process.c
+++ process.c
@@ -6,12 +6,14 @@ int remainingtime;
 int main(int agrc, char * argv[])
 {
     initClk();
-    
+    int remainingTime = atoi(argv[1]);
+    int schdPid = atoi(argv[2]);
+    int schdClk = atoi(argv[3]);
     //TODO it needs to get the remaining time from somewhere
     //remainingtime = ??;
     while (remainingtime > 0)
     {
-        // remainingtime = ??;
+        printf("\nProcess with pid %d started remaining time is %d", getpid(), remainingTime);
     }
     
     destroyClk(false);
--- scheduler.c
+++ scheduler.c
@@ -10,9 +10,7 @@ void runRoundRobin(struct PCBQueue* pcbQ, int timeQuantum, struct PCBQueue* pcbQ
 
 void initializePCB(struct PCBQueue* pcbQ, process p);
 
-void logState(int time, int id, const char *state, PCB *pcb);
 
-void calculatePerformance(PCB *pcbArray);
 
 int main(int argc, char * argv[])
 {
@@ -20,21 +18,10 @@ int main(int argc, char * argv[])
     sch_qid = initSchQueue();
     initClk();
     
-    //algo = atoi(argv[0]);           // algorithm 
-    /*if(argc == 1){
-        quantum = atoi(argv[1]);        //quantum in RR 
-    }*/
-    
-    
-    struct PCBQueue pcbQ;
-    initPCBQueue(&pcbQ);
 
-    struct PCBQueue pcbQDone;
-    initPCBQueue(&pcbQDone);
 
     int currentTime = 0;    
     int receivedProcesses = 0;
-    
     //while loop to maks sure its synced with the proc gen. 
     fflush(stdout);
 
@@ -47,18 +34,33 @@ int main(int argc, char * argv[])
         process p = getProcess(sch_qid);
         if (p.arrivalTime != -1)
         {
-           // PCB for the new process
-           initializePCB(&pcbQ, p);
-           receivedProcesses++;
+            receivedProcesses++;
+            pid_t schdPid = getpid();
+            pid_t prcsPid = fork();
+
+            if(prcsPid == -1) //error happened when forking
+            {
+                perror("Error forking process");
+                exit(EXIT_FAILURE);
+            }
+            else if(prcsPid == 0) //execv'ing to process
+            {
+                char sExecutionTime[5] = {0};
+                char sPid[7] = {0};
+                char sClk[7] = {0};
+                sprintf(sPid, "%d", schdPid);
+                sprintf(sExecutionTime, "%d", 5);
+                sprintf(sClk, "%d", getClk());
+                char *const paramList[] = {"./process.out", sExecutionTime, sPid, sClk,NULL};
+                execv("./process.out", paramList);
+                
+                //if it executes what is under this line, then execv has failed
+                perror("Error in execv'ing to clock");
+                exit(EXIT_FAILURE);
+            }
         }
-
-        // Run Round Robin scheduling algorithm
-        runRoundRobin(&pcbQ, quantum, &pcbQDone);
-
     }
-    while(getQueueSize(&pcbQDone)<NPROC)
-    {runRoundRobin(&pcbQ, quantum, &pcbQDone);}
-    //CAlulate
+
     destroyClk(true);
 }
 
@@ -99,49 +101,150 @@ void initializePCB(struct PCBQueue* pcbQ, process p) {
 
     enqueuePCBQ(pcbQ, pcb);
 }
+#include <stdio.h>
+#include <stdlib.h>
 
-void runRoundRobin(struct PCBQueue* pcbQ, int timeQuantum, struct PCBQueue* pcbQDone)
-{
-   
-    if (pcbQ->front != NULL)
-    {
-        // Get the front process
-        PCB* currentProcess = &pcbQ->front->data;
+typedef struct PCB {
+    // PCB structure definition
+    int id;
+    int pid;
+    int arrivalTime;
+    int priority; // For HPF
+    int executionTime;
+    int remainingTime;
+    int startTime;
+    int finishTime;
+    int waitingTime;
+    int TA;
+    float WTA;
+    int recentStart; // For SRTN
+} PCB;
 
-        // Check if the process is just starting
-        if (currentProcess->startTime == -1 && currentProcess->remainingTime > 0)
-        {
-            currentProcess->startTime = getClk();
-        }
+typedef struct Node {
+    PCB *data;
+    struct Node *next;
+} Node;
 
-        // Run the process for the specified time quantum
-        int remainingTime = currentProcess->remainingTime;
-        int runTime = (remainingTime > timeQuantum) ? timeQuantum : remainingTime;
-        currentProcess->remainingTime -= runTime;
+typedef struct Queue {
+    Node *front;
+    Node *rear;
+    int size;
+} Queue;
 
-        // Print process information
-        printf("\nProcess %d: Arrival Time %d, Run Time %d, Remaining Time %d, Waiting Time %d",
-               currentProcess->id, currentProcess->arrivalTime, currentProcess->runTime,
-               currentProcess->remainingTime, currentProcess->waitingTime);
-
-        // Update waiting time for other processes in the queue
-        struct PCBQNode* temp = pcbQ->front->next;
-        while (temp != NULL)
-        {
-            temp->data.waitingTime += runTime;
-            temp = temp->next;
-        }
-
-        // Check if the process has completed its run
-        if (currentProcess->remainingTime == 0)
-        {
-            currentProcess->turnaroundTime = getClk() - currentProcess->arrivalTime;
-            enqueuePCBQ(pcbQDone, dequeuePCBQ(pcbQ));
-        }
-        else
-        {
-            // Move the process to the end of the queue
-            enqueuePCBQ(pcbQ, dequeuePCBQ(pcbQ));
-        }
+Queue *initializeQueue() {
+    Queue *queue = (Queue *)malloc(sizeof(Queue));
+    if (queue == NULL) {
+        perror("Error initializing queue");
+        exit(EXIT_FAILURE);
     }
+    queue->front = NULL;
+    queue->rear = NULL;
+    queue->size = 0;
+    return queue;
 }
+
+void enqueue(Queue *queue, PCB *data) {
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        perror("Error enqueueing data");
+        exit(EXIT_FAILURE);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (queue->rear == NULL) {
+        queue->front = newNode;
+        queue->rear = newNode;
+    } else {
+        queue->rear->next = newNode;
+        queue->rear = newNode;
+    }
+
+    queue->size++;
+}
+
+PCB *dequeue(Queue *queue) {
+    if (queue->front == NULL) {
+        return NULL;
+    }
+
+    Node *tempNode = queue->front;
+    PCB *tempData = tempNode->data;
+
+    queue->front = queue->front->next;
+    if (queue->front == NULL) {
+        queue->rear = NULL;
+    }
+
+    free(tempNode);
+    queue->size--;
+
+    return tempData;
+}
+
+PCB *peek(Queue *queue) {
+    if (queue->front == NULL) {
+        return NULL;
+    }
+    return queue->front->data;
+}
+
+void printQueue(Queue *queue) {
+    Node *current = queue->front;
+    while (current != NULL) {
+        // Print PCB data or any other relevant information
+        printf("%d ", current->data->id);
+        current = current->next;
+    }
+    printf("\n");
+}
+
+void freeQueue(Queue *queue) {
+    while (queue->front != NULL) {
+        Node *tempNode = queue->front;
+        queue->front = queue->front->next;
+        free(tempNode->data); // Assuming PCB data is dynamically allocated
+        free(tempNode);
+    }
+    free(queue);
+}
+
+int main() {
+    // Example usage of the queue
+
+    Queue *readyQueue = initializeQueue();
+
+    // Example PCB data
+    PCB *process1 = (PCB *)malloc(sizeof(PCB));
+    process1->id = 1;
+    process1->arrivalTime = 0;
+    // Initialize other PCB fields...
+
+    PCB *process2 = (PCB *)malloc(sizeof(PCB));
+    process2->id = 2;
+    process2->arrivalTime = 3;
+    // Initialize other PCB fields...
+
+    enqueue(readyQueue, process1);
+    enqueue(readyQueue, process2);
+
+    printQueue(readyQueue);
+
+    PCB *frontProcess = peek(readyQueue);
+    if (frontProcess != NULL) {
+        printf("Front of the queue: %d\n", frontProcess->id);
+    }
+
+    PCB *dequeuedProcess = dequeue(readyQueue);
+    if (dequeuedProcess != NULL) {
+        printf("Dequeued process: %d\n", dequeuedProcess->id);
+        free(dequeuedProcess); // Free the dequeued PCB data
+    }
+
+    printQueue(readyQueue);
+
+    freeQueue(readyQueue); // Free the entire queue along with PCB data
+
+    return 0;
+}
+
